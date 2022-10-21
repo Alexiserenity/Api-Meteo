2bd1a9579211e0c8e5aaf1159852530ded6a9e14
Alexis Lamaudiere <alexis.lamaudiere@edu.ece.fr>
2022-10-21 19:13:40 +0200

    kjaz

--- js/script.js
+++ js/script.js
@@ -25,11 +25,32 @@ function start()
       document.getElementById('icon-weather-container').innerHTML = icon;
       document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
       
-    }
+    })
+    
+    .getThreeDayforecast()
+    .then(function(response)
+    {
+      let main = new Array(3);
+      let description = new Array(3);
+      let temp = new Array(3);
+      let icon = new Array(3);
+      const i = 0;
+      for(i=0, i<3, i++)
+      {
+        main[i] = data.weather[i].main;      
+        description[i] = data.weather[i].description;
+        temp[i] = data.main.temp;
+        icon[i] = apiWeather.getHTMLElementFromIcon(data.weather[i].icon);
+
+        // Modifier le DOM
+        document.getElementById('today-forecast-main').innerHTML = main;
+        document.getElementById('today-forecast-more-info').innerHTML = description;
+        document.getElementById('icon-weather-container').innerHTML = icon;
+        document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+      }
+    })
     
     
-    
-    )
     .catch(function(error) {
       // Affiche une erreur
       console.error(error);
