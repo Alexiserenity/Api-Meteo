4ec34780ebfe28bc9d2bf849bfebe9bf4a9b4583
Alexis Lamaudiere <alexis.lamaudiere@edu.ece.fr>
2022-10-21 22:24:44 +0200

    ve

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@
 function start() 
 {
   // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
+  apiWeather = null;
+  if(document.getElementById("city-input") && document.getElementById("city-input").value){
+    apiWeather = new API_WEATHER(document.getElementById("city-input").value);
+  }
+  else{
+    apiWeather = new API_WEATHER();
+  }
+
   // Appel par défaut de la strat grace à 'onload'
   apiWeather.onload = function(){start()};
   // Appel de la fonction fetchTodayForecast
@@ -30,24 +37,20 @@ function start()
     .getThreeDayforecast()
     .then(function(response)
     {
-      let main = new Array(3);
-      let description = new Array(3);
-      let temp = new Array(3);
-      let icon = new Array(3);
-      const i = 0;
-      for(i=0, i<3, i++)
-      {
-        main[i] = data.weather[i].main;      
-        description[i] = data.weather[i].description;
-        temp[i] = data.main.temp;
-        icon[i] = apiWeather.getHTMLElementFromIcon(data.weather[i].icon);
-
-        // Modifier le DOM
-        document.getElementById('today-forecast-main').innerHTML = main;
-        document.getElementById('today-forecast-more-info').innerHTML = description;
-        document.getElementById('icon-weather-container').innerHTML = icon;
-        document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+      for(i=0; i<3; i++){
+        // On récupère l'information principal
+        const main = data.list[i].weather[0].main;
+        const description = data.list[i].weather[0].description;
+        const temp = data.list[i].temp.day;
+        const icon = apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
+  
+        // Modifier le DOM2
+        document.getElementById('day'+(i+2)+'-forecast-main').innerHTML = main;
+        document.getElementById('day'+(i+2)+'-forecast-more-info').innerHTML = description;
+        document.getElementById('day'+(i+2)+'-icon-weather-container').innerHTML = icon;
+        document.getElementById('day'+(i+2)+'-forecast-temp').innerHTML = `${temp}°C`;
       }
+  
     })
     
     
